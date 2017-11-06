7a92e9d52de3dc3b1aa7874a683aa47cbd805017
joaobiriba <glaquidara@gmail.com>
2017-11-06 23:59:48 +0100

    Better handle multiple space on trim input name

--- app/src/main/kotlin/com/laquysoft/cleanchucknorrisator/features/changename/ChangedNamePresenter.kt
+++ app/src/main/kotlin/com/laquysoft/cleanchucknorrisator/features/changename/ChangedNamePresenter.kt
@@ -1,5 +1,6 @@
 package com.laquysoft.cleanchucknorrisator.features.changename
 
+import android.util.Log
 import com.laquysoft.cleanchucknorrisator.features.chooser.RandomJokeView
 import javax.inject.Inject
 
@@ -11,7 +12,7 @@ class ChangedNamePresenter
 
     internal lateinit var randomJokeView: RandomJokeView
 
-    val delimiter: String = "\\s+"
+    val delimiter: String = "\\ +"
 
     fun destroy() {
         getChangedNameRandomJoke.dispose()
@@ -19,13 +20,14 @@ class ChangedNamePresenter
     }
 
     fun loadRandomJokeChangedName(textInput: String) {
-        val inputList = textInput.trim().split(delimiter)
+        val inputList = textInput.trim().split(delimiter.toRegex())
         if(inputList.isEmpty()) {
             //TODO: dialog to user
         } else {
             val name = inputList.get(0)
             var surname: String?= null
             if (inputList.size>1) surname = inputList.get(1)
+            Log.d("ChangedNamePresenter", inputList.toString())
             randomJokeView.showLoading()
             getChangedNameRandomJoke.execute(
                     { joke -> randomJokeView.renderJoke(joke)
